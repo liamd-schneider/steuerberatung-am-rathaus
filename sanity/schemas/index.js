33fc054be9be3b35b67ecc51c78f7d1432d1b2a8
Liam Schneider <119621121+liamd-schneider@users.noreply.github.com>
2026-01-26 17:53:14 +0100

    Change customerCounter to customerCounterSchema import

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -3,7 +3,7 @@
 import appointmentSchema from "./appointment-schema";
 import categorySchema from "./category-schema";
 import contactSchema from "./contact-schema";
-import customerCounter from './customerCounter'
+import { customerCounterSchema } from './customerCounter'; // Changed to named import
 import formsSchema from "./forms-schema";
 import notificationSchema from "./notification-schema";
 import { userSchema } from "./user-schema";
@@ -11,6 +11,6 @@ import { userSchema } from "./user-schema";
 
 
 
-const schemas = [contactSchema, userSchema, formsSchema, notificationSchema, appointmentSchema, categorySchema, customerCounter];
+const schemas = [contactSchema, userSchema, formsSchema, notificationSchema, appointmentSchema, categorySchema, customerCounterSchema]; // Updated variable name
 
 export default schemas;
